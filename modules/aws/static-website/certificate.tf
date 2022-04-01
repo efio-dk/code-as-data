336718167a1527489ee918e9b6c90004f257a0ef
Joachim Rorbol <jr@efio.dk>
2022-04-01 17:36:47 +0200

    wip

--- modules/aws/static-website/certificate.tf
+++ modules/aws/static-website/certificate.tf
@@ -11,6 +11,7 @@
 
 resource "aws_acm_certificate" "this" {
   count = local.config.acm_certificate_arn == null ? 1 : 0
+  provider = aws.use1
 
   domain_name       = local.config.domain_name
   validation_method = "DNS"
