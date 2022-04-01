6a2eaddce8321f576bf11b2467b82eb39f4e9402
Joachim Rorbol <jr@efio.dk>
2022-04-01 16:16:28 +0200

    wip

--- modules/aws/vpc/bastion.tf
+++ modules/aws/vpc/bastion.tf
@@ -72,7 +72,7 @@ resource "aws_instance" "bastion" {
     Name = "${local.config.name_prefix}bastion"
   })
 
-user_data =   templatefile("${path.module}/bastion_userdata.sh", {ssh_keys = local.trusted_ssh_public_keys})
+user_data =   templatefile("${path.module}/bastion_userdata.sh", {ssh_keys = local.config.trusted_ssh_public_keys})
 
   root_block_device {
     encrypted = true
