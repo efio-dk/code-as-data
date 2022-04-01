4b23e223ebea1a8ddbcbad18731aa087f1b5bf85
Joachim Rorbol <jr@efio.dk>
2022-04-01 17:24:41 +0200

    wip

--- modules/aws/static-website/certificate.tf
+++ modules/aws/static-website/certificate.tf
@@ -9,11 +9,10 @@
 #   ]
 # }
 
-# resource "aws_acm_certificate" "this" {
-#   # count = length(data.aws_acm_certificate.this) == 0 ? 1 : 0
+resource "aws_acm_certificate" "this" {
+  count = local.config.acm_certificate_arn != null ? 1 : 0
 
-#   domain_name       = local.config.domain_name
-#   validation_method = "DNS"
-#   tags              = local.default_tags
-# }
-
+  domain_name       = local.config.domain_name
+  validation_method = "DNS"
+  tags              = local.default_tags
+}
