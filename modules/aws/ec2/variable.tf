6805e919602dc2450af8186acd8feb0a34748585
Joachim Rorbol <jr@efio.dk>
2022-04-04 21:39:26 +0200

    wip

--- modules/aws/ec2/variable.tf
+++ modules/aws/ec2/variable.tf
@@ -12,6 +12,7 @@ variable "config" {
     ami           = optional(string)
     volumes = optional(set(object({
       device_name = string
+      mountpoint  = string
       size        = number
       type        = optional(string)
       iops        = optional(number)
@@ -26,5 +27,6 @@ variable "config" {
     private_subnets         = set(string)
     public_subnets          = optional(set(string))
     trusted_ssh_public_keys = optional(set(string))
+    kms_key                 = string
   })
 }
