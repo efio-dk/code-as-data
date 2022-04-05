3f4de4b6beb10e5f3d77776ff469e3608de52895
Joachim Rorbol <jr@efio.dk>
2022-04-06 00:48:25 +0200

    bug fix

--- modules/aws/cicd/codepipeline.tf
+++ modules/aws/cicd/codepipeline.tf
@@ -41,7 +41,7 @@ resource "aws_codepipeline" "this" {
       for_each = { for k, v in local.env : k => v if k == each.key && v.source == "codestar" }
 
       content {
-        name             =  "${action.value.owner}/${action.value.repository}?ref=${action.value.branch}"
+        name             =  "${action.value.branch}@${action.value.repository}"
         category         = "Source"
         owner            = "AWS"
         provider         = "CodeStarSourceConnection"
