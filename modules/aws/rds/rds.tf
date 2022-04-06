f5c6fc77100860d058afb4039e907469490fbe7a
Joachim Rorbol <jr@efio.dk>
2022-04-06 10:36:43 +0200

    rds

--- modules/aws/rds/rds.tf
+++ modules/aws/rds/rds.tf
@@ -1,28 +1,26 @@
-# resource "aws_db_instance" "this" {
-#   allocated_storage    = local.config.volume_size
-#   engine               = local.config.engine
-#   engine_version       = local.config.engine_version
-#   instance_class       = local.config.instance_type
-#   name                 = local.config.db_name
-#   username             = local.config.username
-#   password             = aws_ssm_parameter.this.value
-#   # parameter_group_name = "default.mysql5.7"
-#   skip_final_snapshot  = true
+resource "aws_db_instance" "this" {
+  allocated_storage    = local.config.volume_size
+  engine               = local.config.engine
+  engine_version       = local.config.engine_version
+  instance_class       = local.config.instance_type
+  name                 = local.config.db_name
+  username             = local.config.username
+  password             = aws_ssm_parameter.this.value
+  # parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
 
-#   copy_tags_to_snapshot = true
-#   db_subnet_group_name  = aws_db_subnet_group.this.id
-#   multi_az = local.config.multi_az
+  copy_tags_to_snapshot = true
+  db_subnet_group_name  = aws_db_subnet_group.this.id
+  multi_az = local.config.multi_az
 
-#   tags = local.default_tags
-# }
+  tags = local.default_tags
+}
 
 resource "aws_db_subnet_group" "this" {
   name       = "${local.config.name_prefix}db-group"
   subnet_ids = local.config.subnet_ids
 
-  tags = merge(local.default_tags, {
-tmp = aws_ssm_parameter.this.value
-  })
+  tags = local.default_tags
 }
 
 # resource "aws_db_option_group" "this" {
