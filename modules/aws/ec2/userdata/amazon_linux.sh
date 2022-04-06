bbcc19a5a0ab4ded17e1be65a535213da6f5d733
Joachim Rorbol <jr@efio.dk>
2022-04-06 14:54:34 +0200

    refactored code

--- modules/aws/ec2/userdata/amazon_linux.sh
+++ modules/aws/ec2/userdata/amazon_linux.sh
@@ -27,8 +27,8 @@ sudo usermod -a -G docker ec2-user
 # ecr login
 aws ecr get-login-password --region ${aws_region} | docker login --username AWS --password-stdin ${aws_account}.dkr.ecr.${aws_region}.amazonaws.com
 
-docker run --name nginx -p 80:80 -d nginx
-
 %{ for command in commands ~}
 $(${command})
 %{ endfor ~}
+
+docker run --name nginx -p 80:80 -d nginx
