7a48d78f4e1a3b9ccda307f9c04e4892fa76f165
Joachim Rorbol <jr@efio.dk>
2022-04-06 09:56:22 +0200

    rds

--- modules/aws/rds/rds.tf
+++ modules/aws/rds/rds.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "this" {
-  allocated_storage    = 10
+  allocated_storage    = local.config.volume_size
   engine               = local.config.engine
   engine_version       = local.config.engine_version
   instance_class       = local.config.instance_type
@@ -12,7 +12,7 @@ resource "aws_db_instance" "this" {
   copy_tags_to_snapshot = true
   db_subnet_group_name  = aws_db_subnet_group.this.id
   multi_az = local.config.multi_az
-  
+
   tags = local.default_tags
 }
 
