1b9832115a4884e83c956fc702ea942a187f37fd
Joachim Rorbol <jr@efio.dk>
2022-04-06 14:15:07 +0200

    refactored code

--- modules/aws/rds/rds.tf
+++ modules/aws/rds/rds.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "this" {
   engine              = local.config.engine
   engine_version      = local.config.engine_version
   instance_class      = local.config.instance_type
-  name                = local.config.db_name
+  db_name             = local.config.db_name
   username            = local.config.username
   password            = aws_ssm_parameter.this.value
   skip_final_snapshot = true
