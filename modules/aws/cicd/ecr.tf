4736373ded35815a1a46447c0a73b43f3f7fafe2
Joachim Rorbol <jr@efio.dk>
2022-04-07 11:13:58 +0200

    refactored code

--- modules/aws/cicd/ecr.tf
+++ modules/aws/cicd/ecr.tf
@@ -24,34 +24,6 @@ data "aws_iam_policy_document" "ecr" {
   }
 
   // https://docs.aws.amazon.com/AmazonECR/latest/userguide/repository-policy-examples.html
-  /*<<EOF
-{
-    "Version": "2008-10-17",
-    "Statement": [
-        {
-            "Sid": "new policy",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:BatchGetImage",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:PutImage",
-                "ecr:InitiateLayerUpload",
-                "ecr:UploadLayerPart",
-                "ecr:CompleteLayerUpload",
-                "ecr:DescribeRepositories",
-                "ecr:GetRepositoryPolicy",
-                "ecr:ListImages",
-                "ecr:DeleteRepository",
-                "ecr:BatchDeleteImage",
-                "ecr:SetRepositoryPolicy",
-                "ecr:DeleteRepositoryPolicy"
-            ]
-        }
-    ]
-}
-EOF*/
 
 
 }
