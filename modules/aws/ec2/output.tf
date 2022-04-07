f3d7fd2ef6b0a2beb6c96d768b3ef2cfa119f89d
Joachim Rorbol <jr@efio.dk>
2022-04-07 21:14:04 +0200

    refactored code

--- modules/aws/ec2/output.tf
+++ modules/aws/ec2/output.tf
@@ -1,19 +1,19 @@
 output "alb_hostname" {
   description = ""
-  value = local.enable_load_balancer > 0 ? aws_lb.this[0].dns_name : null
+  value       = local.enable_load_balancer > 0 ? aws_lb.this[0].dns_name : null
 }
 
 output "alb_arn" {
   description = ""
-  value = local.enable_load_balancer > 0 ? aws_lb.this[0].arn : null
+  value       = local.enable_load_balancer > 0 ? aws_lb.this[0].arn : null
 }
 
 output "alb_ecurity_group_id" {
   description = ""
-  value = local.enable_load_balancer > 0 ? aws_security_group[0].alb.id : null
+  value       = local.enable_load_balancer > 0 ? aws_security_group[0].alb.id : null
 }
 
 output "iam_role_arn" {
   description = ""
-  value = aws_iam_role.this.arn 
+  value       = aws_iam_role.this.arn
 }
