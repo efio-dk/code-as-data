40545dd3192b197828623ef865e67146e1d85170
Joachim Rorbol <jr@efio.dk>
2022-04-07 22:26:16 +0200

    refactored code

--- modules/aws/static-website/main.tf
+++ modules/aws/static-website/main.tf
@@ -7,6 +7,6 @@ locals {
     disable_cloudfront     = false
   })
 
-  provision_kms = local.config.kms_arn != null ? 1 : 0
+  provision_kms = local.config.kms_arn == null ? 1 : 0
   kms_arn       = try(aws_kms_key.this[0].arn, local.config.kms_arn)
 }
