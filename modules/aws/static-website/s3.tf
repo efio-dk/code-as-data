44ec4518c7913deed3b72acc78f5298bdfe58b8c
Joachim Rorbol <jr@efio.dk>
2022-05-04 09:26:14 +0200

    wip

--- modules/aws/static-website/s3.tf
+++ modules/aws/static-website/s3.tf
@@ -29,12 +29,12 @@ resource "aws_s3_bucket_public_access_block" "this" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
+/*
 resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.this.id
   acl    = "private"
 }
-
+*/
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
