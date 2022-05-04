ff8401eaf59bf7f054709225b4f244049484c6fa
Joachim Rorbol <jr@efio.dk>
2022-05-04 11:31:28 +0200

    wip

--- modules/aws/static-website/cloudfront.tf
+++ modules/aws/static-website/cloudfront.tf
@@ -11,7 +11,7 @@ resource "aws_cloudfront_distribution" "this" {
   is_ipv6_enabled     = true
   price_class         = "PriceClass_100"
   default_root_object = local.config.index_document
-  aliases             = concat([local.config.domain_name], tolist(local.config.domain_alias))
+  aliases             = aws_acm_certificate.this[0].status != "ISSUED" ? [] : concat([local.config.domain_name], tolist(local.config.domain_alias))
   wait_for_deployment = false
   comment             = "Cloudfront CDN for ${local.name_prefix}website"
   tags                = local.default_tags
