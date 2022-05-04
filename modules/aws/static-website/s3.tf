fe10a1095cce8ce8963bb1e85ddc60f8a08f69d1
Joachim Rorbol <jr@efio.dk>
2022-05-04 15:00:54 +0200

    wip

--- modules/aws/static-website/s3.tf
+++ modules/aws/static-website/s3.tf
@@ -24,10 +24,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls       = true
-  block_public_policy     = true
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = true
+  block_public_policy     = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_acl" "this" {
