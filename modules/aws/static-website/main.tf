cd70024f32987b1d85260f2be7a9465cf8b465cd
Joachim Rorbol <jr@efio.dk>
2022-05-04 12:39:53 +0200

    wip

--- modules/aws/static-website/main.tf
+++ modules/aws/static-website/main.tf
@@ -1,10 +1,9 @@
 locals {
   config = defaults(var.config, {
-    index_document         = "index.html"
-    error_document         = "index.html"
-    deploy_sample_document = false
-    cache_policy           = "CachingOptimized"
-    disable_cloudfront     = false
+    index_document     = "index.html"
+    error_document     = "index.html"
+    cache_policy       = "CachingOptimized"
+    disable_cloudfront = false
   })
 
   provision_kms = local.config.kms_arn == null ? 1 : 0
