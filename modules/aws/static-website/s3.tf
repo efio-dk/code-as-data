9490e5dfd39889ba1c749fcefc39d93d3212738a
Joachim Rorbol <jr@efio.dk>
2022-05-04 14:51:32 +0200

    wip

--- modules/aws/static-website/s3.tf
+++ modules/aws/static-website/s3.tf
@@ -20,7 +20,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
     }
   }
 }
-/*
+
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
@@ -34,7 +34,7 @@ resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.this.id
   acl    = "private"
 }
-*/
+
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
