0a963a58358c8fd3eee3ab23fe80e4023f2866b9
Joachim Rørbøl <Rorbol@users.noreply.github.com>
2022-05-16 20:34:09 +0200

    Enabling EC2 module https & adding cognito params (#13)

--- modules/aws/ec2/alb.tf
+++ modules/aws/ec2/alb.tf
@@ -73,34 +73,32 @@ resource "aws_lb_target_group" "this" {
   })
 }
 
-# resource "aws_lb_listener" "this" {
-#   count = local.enable_load_balancer
-
-#   load_balancer_arn = aws_lb.this[0].arn
-#   port              = "80"
-#   protocol          = "HTTP"
-
-#   default_action {
-#     type = "redirect"
-
-#     redirect {
-#       port        = "443"
-#       protocol    = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   }
-# }
-
-resource "aws_lb_listener" "this" {
+resource "aws_lb_listener" "http" {
   count = local.enable_load_balancer
 
   load_balancer_arn = aws_lb.this[0].arn
-  # port              = "443"
-  # protocol          = "HTTPS"
-  port     = "80"
-  protocol = "HTTP"
-  # ssl_policy        = "ELBSecurityPolicy-2016-08"
-  # certificate_arn = aws_acm_certificate.this.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  count = local.enable_load_balancer
+
+  load_balancer_arn = aws_lb.this[0].arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = local.certificate_arn
 
   default_action {
     type             = "forward"
